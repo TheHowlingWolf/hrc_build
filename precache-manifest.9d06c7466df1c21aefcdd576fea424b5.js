de5e036a0b2cbf1e69610f105e30075834fbbb6b
Debraj Sengupta <1705232@kiit.ac.in>
2020-05-05 03:52:10 +0530

    Initial commit

---
+++ precache-manifest.9d06c7466df1c21aefcdd576fea424b5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b3b05bc5b7f99d3ddb037e9f719678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a166d3133844622da5c",
+    "url": "/static/css/main.da9aeb1d.chunk.css"
+  },
+  {
+    "revision": "9e48b6ebc7e9030394e5",
+    "url": "/static/js/2.e7d2f951.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e7d2f951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a166d3133844622da5c",
+    "url": "/static/js/main.c81836ad.chunk.js"
+  },
+  {
+    "revision": "1c6bdf825c6f61f66e65",
+    "url": "/static/js/runtime-main.66891d28.js"
+  },
+  {
+    "revision": "c66b572dab7087c044a7139b52374e7c",
+    "url": "/static/media/01.c66b572d.jpg"
+  },
+  {
+    "revision": "b212d77317c2140cc380648fcef183ef",
+    "url": "/static/media/02.b212d773.jpg"
+  },
+  {
+    "revision": "e97f7d1597ffbce8a6d43a1de203a4f8",
+    "url": "/static/media/03.e97f7d15.jpg"
+  },
+  {
+    "revision": "240c9db87f5e3655f2c39349c50af939",
+    "url": "/static/media/ElProf.240c9db8.jpg"
+  },
+  {
+    "revision": "44ad94a5de3cb641706b0341ab56d944",
+    "url": "/static/media/InkedOptimized-soham_LI.44ad94a5.jpg"
+  },
+  {
+    "revision": "e8188c12fcd907b55e4c999921adb59e",
+    "url": "/static/media/art-educational-paper-phrase-142815.e8188c12.jpg"
+  },
+  {
+    "revision": "fac643b65f65799dc753d11da2f164f5",
+    "url": "/static/media/cardBack.fac643b6.jpg"
+  },
+  {
+    "revision": "9155a9e9889b29cc23ae69bf2a9d7433",
+    "url": "/static/media/f2.9155a9e9.png"
+  },
+  {
+    "revision": "b8eeb1187cde5695cd51fb4a220d4e57",
+    "url": "/static/media/f3.b8eeb118.png"
+  },
+  {
+    "revision": "c0891edea3f64b162ec59bc9adec512f",
+    "url": "/static/media/naturephotography.c0891ede.jpg"
+  },
+  {
+    "revision": "4628cfd1ca2c06715fb96c40bee4feb3",
+    "url": "/static/media/unnamed.4628cfd1.png"
+  }
+]);
